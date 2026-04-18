17b2b8f9c3273c4dd54666c85ace19b2982391db
Armish <shkarmish@gmail.com>
2026-04-19 02:27:38 +0500

    Added Tailwind props project

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  return (
+    <div>
+<Card username="Armish" btnText="Apply Now"/>
+    </div>
+  )
+}
+
+export default App
